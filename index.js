c24a22efc15bb8df9ee8fa12e78de41e496c9451
josel82 <jose.padilla.dev@gmail.com>
2022-08-25 23:23:23 +0100

    fixed bug in index.js file

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require("express");
-const redis = require(redis);
+const redis = require("redis");
 
 const port = 8081
 const app = express();
-const client = redis.createClient();
+const client = redis.createClient({
+    host: 'redis-server',
+    port: 6379
+});
 
 client.set('visits', 0);
 
 app.get('/',(req, res)=>{
     client.get('visits', (err, visits)=>{
-        res.send('Number of visits is'+ visits)
+        res.send('Number of visits is '+ visits)
         client.set('visits', parseInt(visits) + 1)
     });
 });
